1b8a899290f5be3ad11069440de20720c10fa51e
Abdul Motaleb <71353762+learnwithmotaleb@users.noreply.github.com>
2026-06-16 03:51:17 +0600

    message

--- lib/python3127/Lib/site-packages/windows/native_exec/__init__.py
+++
@@ -1,3 +0,0 @@
-from .native_function import create_function
-
-__all__ = ["create_function"]
--- lib/python3127/Lib/site-packages/windows/winproxy/__init__.py
+++
@@ -1,3 +0,0 @@
-from .apiproxy import is_implemented, get_target, resolve
-from .error import WinproxyError, ExportNotFound
-from .apis import * # Import all functions
